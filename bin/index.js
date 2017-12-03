942add2c3ba09bc3954edd5c0833f37ce444f944
Matt Carroll <matt.carroll139@gmail.com>
2017-12-04 10:04:45 +1100

    Fixed meow options

--- bin/index.js
+++ bin/index.js
@@ -14,27 +14,37 @@ const help = require('../lib/help');
 
 const cli = meow({
   help,
-  version: true
-}, {
-  boolean: [
-    'force',
-    'unset',
-    'debug',
-    'release',
-    'upload' // meow will also allow --no-upload from cmd line
-  ],
-  default: {
-    debug: true,
-    release: false,
-    force: false,
-    upload: true,
-    type: 'cordova'
-  },
-  string: [
-    'platforms',
-    'notify',
-    'type'
-  ]
+  flags: {
+    'force': {
+      type: 'boolean',
+      default: false
+    },
+    'unset': {
+      type: 'boolean'
+    },
+    'debug': {
+      type: 'boolean',
+      default: true
+    },
+    'release': {
+      type: 'boolean',
+      default: false
+    },
+    'upload': { // meow will also allow --no-upload from cmd line
+      type: 'boolean',
+      default: true
+    },
+    'platforms': {
+      type: 'string'
+    },
+    'notify': {
+      type: 'string'
+    },
+    'type': {
+      type: 'string',
+      default: 'cordova'
+    }
+  }
 });
 
 main(cli.input, cli.flags);
